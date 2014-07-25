dc6f23fea7e06f329357f204f5a9a818c1d66540
Pakillo <f.rodriguez.sanc@gmail.com>
2014-07-25 17:34:09 +0200

    Initial version 0.1. Contains one generic rmarkdown template with citations support

---
+++ R/rmdTemplates.R
@@ -0,0 +1,19 @@
+#' A collection of Rmarkdown templates. Include helpful defaults for
+#' knitr options, as well as support for citations and different bibliography styles.
+#' 
+#' After installing the package, click on `create new Rmarkdown document` in Rstudio
+#' to have access to the templates. 
+#' See \link{http://rmarkdown.rstudio.com/developer_document_templates.html} for 
+#' more information.
+#' 
+#'
+#' @docType package
+#' @name rmdTemplates
+#' @title A collection of Rmarkdown templates
+#' @import knitr
+#' @import knitcitations
+#' @import rmarkdown
+#' @author F. Rodriguez-Sanchez \email{f.rodriguez.sanc@@gmail.com}
+#' @keywords package
+#'
+NULL
---
+++ README.md
@@ -0,0 +1,12 @@
+## Rmarkdown templates
+
+An R package containing a collection of Rmarkdown templates. Includes helpful defaults for knitr options, as well as support for in-text citations and different bibliography styles. See [here](http://pakillo.github.io/Rmd_templates/) for an example.
+
+After installing the package, click on `create new Rmarkdown document` in Rstudio
+to have access to the templates. 
+
+See http://rmarkdown.rstudio.com/developer_document_templates.html for 
+more information.
+
+Thanks to Yihui Xie, Carl Boettiger, the Rstudio team, and all the people developing and contributing to this workflow.
+
